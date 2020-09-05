d87b984f50404d8b047f7ecb46b2926c93bcafc6
tizsh <70004278+tizsh@users.noreply.github.com>
2020-09-05 19:42:13 -0400

    Update core.py

--- Hagia Sophia/front-end infrastruct/hagia/sophia/core.py
+++ Hagia Sophia/front-end infrastruct/hagia/sophia/core.py
@@ -8,19 +8,12 @@ import time
 from .models import Bookmark, Folder
 
 
-
-#these are constants 
-
 basic_url_pattern = "([a-zA-Z0-9]*[\.*])*[a-zA-Z0-9]+[\.][a-zA-Z]+([a-zA-Z0-9\.\&\/\?\:@\-_=#])*"
 proper_url_pattern = "((http|https)\:\/\/)+[a-zA-Z0-9\.\/\?\:@\-_=#]+\.([a-zA-Z]){2,6}([a-zA-Z0-9\.\&\/\?\:@\-_=#])*"
 
 DESCRIPTION_MAX = 100
 TITLE_MAX = 20
-DEFAULT_FOLDER = "///none///"
-
-# ignore_invalid_urls = True
-
-#these are constants
+DEFAULT_FOLDER_ID = 0 
 
 
 def match_url(pattern, string):
@@ -47,10 +40,9 @@ def get_description(soup):
 	return ""
 
 
-def folder_exist(name1):
+def folder_exist(primary_key):
 	try:
-		name1 =	name1.replace("%20", " ")
-		folder = Folder.objects.get(name = name1)
+		folder = Folder.objects.get(pk = primary_key)
 		if(folder == None):
 			return False
 		else:
@@ -61,7 +53,7 @@ def folder_exist(name1):
 	return False
 
 
-def add_bookmark_to_db(url, title = None, description = None, folder_name = DEFAULT_FOLDER):
+def add_bookmark_to_db(url, title = None, description = None, folder_primary_key = DEFAULT_FOLDER_ID):
 	if(title == None):
 		title = url
 
@@ -73,14 +65,11 @@ def add_bookmark_to_db(url, title = None, description = None, folder_name = DEFA
 
 	if(len(title) > TITLE_MAX):
 		title = title[:TITLE_MAX - 3] + "..."
-
-	if(description == None):
-		description = ""
 	
 	if(len(description) > DESCRIPTION_MAX):
 		description = description[:DESCRIPTION_MAX - 3] + "..."
 
-	fol = Folder.objects.get(name = folder_name)
+	fol = Folder.objects.get(pk = folder_primary_key)
 	bm = Bookmark(url = url, title = title, description = description, folder_name = fol)
 	bm.save()
 
@@ -105,7 +94,7 @@ def normalize(l):
 	return l
 
 
-def add_online(link, folder_name = None):
+def add_online(link, folder_primary_key = None):
 
 	if (match_url(proper_url_pattern, link) == False and match_url(basic_url_pattern,link) == True):
 		link = "http://"+link
@@ -121,23 +110,23 @@ def add_online(link, folder_name = None):
 	title = soup.title.text
 	description = get_description(soup)
 	
-	if(folder_exist(folder_name) == True and folder_name != None):
-		add_bookmark_to_db(url = link, title = title, description = description, folder_name = folder_name)
+	if(folder_exist(folder_primary_key) == True and folder_primary_key != None):
+		add_bookmark_to_db(url = link, title = title, description = description, folder_primary_key = folder_primary_key)
 		return True
 
-	if(folder_exist(folder_name) == False and folder_name != None):
-		add_folder_to_db(folder_name)
-		add_bookmark_to_db(url = link, title = title, description = description, folder_name = folder_name)
+	if(folder_exist(folder_primary_key) == False and folder_primary_key != None):
+		add_folder_to_db(folder_primary_key)
+		add_bookmark_to_db(url = link, title = title, description = description, folder_primary_key = folder_primary_key)
 		return True
 
 	else:
-		add_bookmark_to_db(url = link, title = title, description = description, folder_name = DEFAULT_FOLDER)
+		add_bookmark_to_db(url = link, title = title, description = description, folder_primary_key = DEFAULT_FOLDER_ID)
 		return True
 
 	return False
 
 
-def offline_add(link, folder_name = None):
+def offline_add(link, folder_primary_key = None):
 
 	if (match_url(proper_url_pattern, link) == False and match_url(basic_url_pattern,link) == True):
 		link = "http://"+link
@@ -149,15 +138,9 @@ def offline_add(link, folder_name = None):
 		return False
 
 	try:
-		if(folder_exist(folder_name) == True and folder_name != None):
-			add_bookmark_to_db(url = link, folder_name = folder_name)
+		if(folder_exist(folder_primary_key) == True and folder_primary_key != None):
+			add_bookmark_to_db(url = link, folder_primary_key = folder_primary_key)
 			return True
-
-		if(folder_exist(folder_name) == False and folder_name != None):
-			add_folder_to_db(folder_name)
-			add_bookmark_to_db(url = link, folder_name = folder_name)
-			return True
-
 		else:
 			add_bookmark_to_db(url = link)
 			return True
@@ -168,18 +151,16 @@ def offline_add(link, folder_name = None):
 	return False
 
 
-
-def add_bookmark(link,folder_name = None):
+def add_bookmark(link,folder_primary_key = None):
 	link = normalize(link)
 	try:
-		return add_online(link, folder_name)
+		return add_online(link, folder_primary_key)
 
 	except Exception as e:
 		print("add_online failed... trying offline method")
 
 	try:
-		delete_bookmark("http://hello.com/fuckniggers101")
-		return offline_add(link, folder_name)
+		return offline_add(link, folder_primary_key)
 	
 	except Exception as e:
 		print("offline_add failed... returning false")
@@ -187,19 +168,17 @@ def add_bookmark(link,folder_name = None):
 	return False
 
 
-### Remember that bookmark's url are not unique! To precisely delete the correct
-
-def delete_folder(fol, delete_inner_bookmarks = False):
+def delete_folder(folder_primary_key, delete_inner_bookmarks = False):
 	try:
 
 		if(delete_inner_bookmarks == True):
-			bm = Bookmark.objects.filter(folder_name = Folder.objects.get(name = fol))
+			bm = Bookmark.objects.filter(folder_name = Folder.objects.get(pk = folder_primary_key))
 			bm.delete()
 			bm.save()
 
 		else:
-			bm = Bookmark.objects.filter(folder_name = Folder.objects.get(name = fol))
-			bm.update(folder_name = Folder.objects.get(name = fol))
+			bm = Bookmark.objects.filter(folder_name = Folder.objects.get(pk = folder_primary_key))
+			bm.update(folder_name = Folder.objects.get(pk = DEFAULT_FOLDER_ID))
 			bm.save()
 
 		folder = Folder.objects.filter(name = fol)
@@ -223,23 +202,5 @@ def delete_bookmark(link):
 		print(e)
 
 	return False
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
 
 
